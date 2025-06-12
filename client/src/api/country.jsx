0a942ba040a6ca397265be4425d1ef865f231a58
Reda_Peeraphat <peeraphats2000@gmail.com>
2025-06-12 22:41:03 +0700

    changeAPI

--- client/src/api/country.jsx
+++ client/src/api/country.jsx
@@ -1,6 +1,8 @@
 import axios from 'axios';
 
-export const CountryPerYear = async (year) => await axios.get('http://localhost:5001/api/country/' + year)
+// export const CountryPerYear = async (year) => await axios.get('http://localhost:5001/api/country/' + year)
+export const CountryPerYear = async (year) => await axios.get('https://world-population-chart.onrender.com/api/country/' + year)
 
-export const PopulationPerYear = async (year) => await axios.get('http://localhost:5001/api/population/' + year)
+// export const PopulationPerYear = async (year) => await axios.get('http://localhost:5001/api/population/' + year)
+export const PopulationPerYear = async (year) => await axios.get('https://world-population-chart.onrender.com/api/population/' + year)
 
