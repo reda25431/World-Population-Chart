bd7197a190ab1b50d641f73f82c6b4d95edec452
Reda_Peeraphat <peeraphats2000@gmail.com>
2025-06-12 19:29:44 +0700

    success code

--- server/config/connect_data.js
+++ server/config/connect_data.js
@@ -2,20 +2,6 @@ const mysql = require('mysql2/promise');
 
 require('dotenv').config()
 
-// const dbConfig = {
-//     host: 'localhost',
-//     user: 'root',
-//     password: 'root',
-//     database: 'population_db',
-//     waitForConnections: true,
-//     connectionLimit: 10,
-//     queueLimit: 0,
-//     acquireTimeout: 60000,
-//     timeout: 60000,
-//     reconnect: true
-// };
-
-// const pool = mysql.createPool(dbConfig);
 const pool = mysql.createPool(process.env.DATABASE_URL);
 
 module.exports = pool;
