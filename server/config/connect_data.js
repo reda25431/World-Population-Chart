aa33dcdf55e50605c993d227141212f47c9b29af
Reda_Peeraphat <peeraphats2000@gmail.com>
2025-06-12 22:32:14 +0700

    changeconnect

--- server/config/connect_data.js
+++ server/config/connect_data.js
@@ -1,7 +1,23 @@
+// const mysql = require('mysql2/promise');
+
+// require('dotenv').config()
+
+// const pool = mysql.createPool(process.env.DATABASE_URL);
+
+// module.exports = pool;
+//-------------------------------------------------//
 const mysql = require('mysql2/promise');
+require('dotenv').config();
 
-require('dotenv').config()
-
-const pool = mysql.createPool(process.env.DATABASE_URL);
+const pool = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
+  ssl: {
+    rejectUnauthorized: true
+  }
+});
 
 module.exports = pool;
